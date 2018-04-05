67cb4ec67170da724a9ce7796177d7d17952dd23
peter97silva <pedrosilva97@gmail.com>
2018-04-05 13:10:55 +0100

    Superclasse Pessoa

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,41 +1,11 @@
 import java.util.LinkedList;
 
-public class Aluno extends Identificador {
+public class Aluno extends Pessoa {
 
-    private LinkedList<Aula> aulas;
 
     public Aluno(String nome, long numero) {
         super(nome, numero);
-        this.aulas = new LinkedList<>();
     }
 
-    public void adicionar(Aula aula) {
-        aulas.add(aula);
-    }
-
-    public void remover(Aula aula) {
-        aulas.remove(aula);
-    }
-
-    public LinkedList<Aula> getAulas() {
-        return new LinkedList<Aula>(aulas);
-    }
-
-    public void preencherSumario(Aula aula) {
-        if (aulas.contains(aula)) {
-            aula.adicionarLinhaSumario(nome);
-        }
-    }
-
-    public LinkedList<Aula> getAulas(Horario horario) {
-
-        LinkedList<Aula> aulasHorario = new LinkedList<>();
-        for (Aula aula : aulas) {
-            if (aula.getHorario().isSobreposto(horario)) {
-                aulasHorario.add(aula);
-            }
-        }
-        return aulasHorario;
-    }
 
 }
--- src/Professor.java
+++ src/Professor.java
@@ -1,56 +1,9 @@
 import java.util.LinkedList;
 
-public class Professor extends Identificador {
-
-    private LinkedList<Aula> aulas;
+public class Professor extends Pessoa {
 
     public Professor(String nome, long numero) {
         super(nome, numero);
-        this.aulas = new LinkedList<>();
-    }
-
-
-    public LinkedList<Aula> getAulas() {
-        return new LinkedList<Aula>(aulas);
-    }
-
-    public LinkedList<Aula> getAulas(Horario horario) {
-
-        LinkedList<Aula> aulasHorario = new LinkedList<>();
-        for (Aula aula : aulas) {
-            if (aula.getHorario().isSobreposto(horario)) {
-                aulasHorario.add(aula);
-            }
-        }
-        return aulasHorario;
-    }
-
-    public void setNumero(long numero) {
-        this.numero = numero;
-    }
-
-
-    public void preencherSumario(Aula aula) {
-        if (!aulas.contains(aula)) {
-            return;
-        }
-        aula.adicionarLinhaSumario(aula.getNome());
-        aula.adicionarLinhaSumario(String.valueOf(aula.getNumero()));
-        aula.adicionarLinhaSumario(nome);
-        for (Aluno aluno : aula.getAlunos()) {
-            aluno.preencherSumario(aula);
-        }
-    }
-
-
-    public void adicionar(Aula aula) {
-        aulas.add(aula);
-    }
-
-    public void remover(Aula aula) {
-        if (aulas.contains(aula)) {
-            aulas.remove(aula);
-        }
     }
 
 
